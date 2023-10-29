74ff4e2757809acc5db5fe17eecd719c503e0a14
Yury Vereskun <satriks@mail.ru>
2023-10-29 03:27:51 +0300

    lint

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,8 @@
-import Game from "./game_widget/game";
+import Game from './game_widget/game'
 
-const element = document.querySelector('.game');
-const url = 'http://localhost:7070/sse';
+const element = document.querySelector('.game')
+const url = 'http://localhost:7070/sse'
 
-const game = new Game(element, url);
+const game = new Game(element, url) // eslint-disable-line
+
 // game.start();
