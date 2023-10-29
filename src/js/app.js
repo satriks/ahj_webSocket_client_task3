15da25030534e2436be01a29aa0ebd4f373f44d0
Yury Vereskun <satriks@mail.ru>
2023-10-29 03:40:17 +0300

    added server

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,8 @@
 import Game from './game_widget/game'
 
 const element = document.querySelector('.game')
-const url = 'http://localhost:7070/sse'
+// const url = 'http://localhost:7070/sse'
+const url = 'https://ahj-websocket-server-task3.onrender.com/sse'
 
 const game = new Game(element, url) // eslint-disable-line
 
